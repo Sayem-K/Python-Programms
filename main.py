6c083017f1c088926df17946f95144bd7a991a0e
hoigoysohio <njuli@yahoo.com>
2024-11-28 18:58:50 +0000

    completed

---
+++ main.py
@@ -0,0 +1,20 @@
+("Welcome to Prograaming!")
+(100)
+
+print("I want to add line break!\n")
+print("I added line break.")
+
+print(" I want to add tab!\t")
+print("I added tab.")
+
+print("What is 658 -  489?","10")
+
+answer = input("What is the name of the body of water near Georgia? ")
+if answer == "Black Sea":
+    print ("Nice!",answer ,"was correct. You can now see the keyword list!\n")
+else:
+    print("Try again!")
+
+import keyword
+
+print(keyword.kwlist)
